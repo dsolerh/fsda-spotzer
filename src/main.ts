285d815b5571663aee88ff455164c2ce5cb5723d
dsolerh <dsolerh.cinter95@gmail.com>
2022-01-15 19:00:59 -0500

    add docs to the project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { resolve } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const pkgPath = resolve('', 'package.json');
+  const pkg = JSON.parse(
+    await readFile(pkgPath).then((buff) => buff.toString()),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Spotze API')
+    .setDescription("Accepts orders from Company X's Partners")
+    .setVersion(pkg['version'])
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT);
 }
 bootstrap();
