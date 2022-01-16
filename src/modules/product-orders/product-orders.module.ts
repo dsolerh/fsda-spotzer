dd6c451903ddd7454bb5228d41ab53b6f9c254d2
dsolerh <dsolerh.cinter95@gmail.com>
2022-01-15 19:05:42 -0500

    fix path to relative

--- src/modules/product-orders/product-orders.module.ts
+++ src/modules/product-orders/product-orders.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ProductOrdersService } from './product-orders.service';
 import { ProductOrdersController } from './product-orders.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { OrderRepository } from 'src/models/order.repository';
+import { OrderRepository } from './entity/order.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([OrderRepository])],
