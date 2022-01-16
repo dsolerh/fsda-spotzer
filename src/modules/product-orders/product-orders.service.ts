94ed0cdd022d4b743ea7d81d7657cbf14f06ec82
dsolerh <dsolerh.cinter95@gmail.com>
2022-01-15 19:09:16 -0500

    add validation to service

--- src/modules/product-orders/product-orders.service.ts
+++ src/modules/product-orders/product-orders.service.ts
@@ -1,22 +1,58 @@
-import { BadRequestException, Injectable, Logger } from '@nestjs/common';
-import { validateOrReject } from 'class-validator';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
 import { OrderEntity } from './entity/order.entity';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { OrderRepository } from './entity/order.repository';
+import { plainToClass } from 'class-transformer';
+import { isNotEmpty, validateOrReject } from 'class-validator';
+import { ProductTypeEnum } from './constants/product-type.enum';
 
 @Injectable()
 export class ProductOrdersService {
-  protected logger: Logger;
-  constructor(private readonly repo: OrderRepository) {
-    this.logger = new Logger();
-  }
-  async create(createOrderDto: CreateOrderDto): Promise<OrderEntity> {
+  protected logger = new Logger();
+  constructor(private readonly repo: OrderRepository) {}
+  async create(dto: CreateOrderDto): Promise<OrderEntity> {
+    let order: OrderEntity;
     try {
-      await validateOrReject(createOrderDto);
+      dto = await this.validateDto(dto);
     } catch (errors) {
-      this.logger.error('(validation failed). Errors: ', errors);
+      this.logger.error('(Validation failed). Errors: ', errors);
       throw new BadRequestException(errors);
     }
-    return this.repo.create(createOrderDto);
+    try {
+      order = await this.repo.transactionalCreate(dto);
+    } catch (errors) {
+      this.logger.error('(Creation failed). Errors: ', errors);
+      throw new InternalServerErrorException(errors);
+    }
+    return order;
+  }
+
+  private async validateDto(rawDto: any): Promise<CreateOrderDto> {
+    const dto = plainToClass(CreateOrderDto, rawDto);
+    await validateOrReject(dto);
+    for (const product of dto.LineItems) {
+      if (
+        product.ProductType === ProductTypeEnum.PAIDSEARCH &&
+        isNotEmpty(product.WebsiteDetails)
+      ) {
+        throw new BadRequestException(
+          `Invalid argument 'WebsiteDetails' for a Product with 'ProductType'=${product.ProductType}`,
+        );
+      }
+      if (
+        product.ProductType === ProductTypeEnum.WEBSITE &&
+        isNotEmpty(product.AdWordCampaign)
+      ) {
+        throw new BadRequestException(
+          `Invalid argument 'AdWordCampaign' for a Product with 'ProductType'=${product.ProductType}`,
+        );
+      }
+    }
+    return dto;
   }
 }
